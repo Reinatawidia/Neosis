1beb3871fab3da0e22da3993ec4c4fc383df7be7
Nico-Siringoringo <arthurhirai255@gmail.com>
2025-11-24 20:06:07 +0700

    project setup

---
+++ drizzle.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "drizzle-kit";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: ".env.local"
+})
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./configs/schema.js",
+  dbCredentials : {
+    url: process.env.NEXT_PUBLIC_DATABASE_CONNECTION_STRING,
+  },
+});
